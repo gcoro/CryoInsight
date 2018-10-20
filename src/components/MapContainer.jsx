f692a816190f0166dd2a32b79cd544ddcf664ebe
Davide Trevisan <davide.trevisan@ennova-research.com>
2018-10-20 14:49:13 +0200

    leaflet integration

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+var L = require('leaflet');
 
 export default class MapContainer extends React.Component {
 	render() {
 		return (<div className='map-container'>
-			<p>I'm the map</p>
+			<div style={{ 'height': 'calc(100vh - 80px)' }} id='mapid'></div>
 		</div>);
+	}
+	componentDidMount(){
+		var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+		L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.satellite',
+    accessToken: 'pk.eyJ1IjoidHJleHkiLCJhIjoiY2puaGY5aHh5MGRlcjNwcXpkY204cnlnYiJ9.YlB5RoXU8xIZUuAwBdgVFQ'
+}).addTo(mymap);
 	}
 }

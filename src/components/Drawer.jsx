fd414cd6316ba5cf077adbb499ee6ece65257a64
Giulia <giulia.coro@ennova-research.com>
2018-10-20 15:38:38 +0200

    Added some basic style

--- src/components/Drawer.jsx
+++ src/components/Drawer.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 export default class Drawer extends React.Component {
 	render() {
-		return (<div style={{ position: 'absolute', zIndex: '1000', width: '30vh', height: 'calc(100vh - 80px)', background: 'dodgerblue' }}>
+		return (<div style={{
+			position: 'absolute', zIndex: '1001', width: '30vh', height: 'calc(100vh - 80px)',
+			background: 'dodgerblue'
+		}}>
 
-		<input id='input-location' />
-		<p>Drawer field</p>
+			<p>Drawer field</p>
 		</div>);
 	}
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,27 @@ import React from 'react';
 
 export default class Header extends React.Component {
 	render() {
-		return (<div className='header' style={{background: 'dodgerblue', height: '80px'}}>
-			<button onClick={this.props.handleOpenDrawer}>Toggle toolbar</button>Cryoinsight 
+		return (<div className='header' style={{
+			background: 'dodgerblue', height: '80px',
+			display: 'flex', justifyContent: 'space-between', alignItems: 'center'
+		}}>
+			<button onClick={this.props.handleOpenDrawer} style={{
+				color: 'white', background: 'none',
+				border: 'none', outline: 'none', fontSize: '30px', paddingLeft: '10px'
+			}}>
+				<i className="fas fa-chevron-circle-right"></i>
+			</button>
+			<p style={{
+				fontFamily: '\'Niramit\', sans-serif', color: 'white',
+				fontWeight: 'bold', fontSize: '30px'
+			}}>Cryoinsight</p>
+			<div id='search-bar'>
+				<input id='input-location' style={{
+					height: '35px', marginRight: '10px',
+					paddingRight: '30px', paddingLeft: '5px'
+				}} />
+				<i className="fas fa-search" style={{ position: 'relative', right: '35px' }} />
+			</div>
 		</div>);
 	}
 }

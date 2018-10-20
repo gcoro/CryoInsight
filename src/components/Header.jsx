fe368f8e355e29217e55bb2f0bb03061bd08dbfd
Giovanni <giovanni.motterle@yahoo.it>
2018-10-20 19:41:30 +0200

    search on enter

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,35 +2,42 @@ import React from 'react';
 const logo = require('../assets/images/logo.svg');
 
 export default class Header extends React.Component {
-	render() {
-		return <>
-			<div className='header' style={{
-				background: 'dodgerblue', height: '80px',
-				display: 'flex', justifyContent: 'space-between', alignItems: 'center'
-			}}>
-				<button onClick={this.props.handleOpenDrawer} style={{
-					color: 'white', background: 'none',
-					border: 'none', outline: 'none', fontSize: '30px', paddingLeft: '10px'
-				}}>
-					{(this.props.drawerIsOpen && <i className="fas fa-chevron-circle-down"></i>) ||
-						<i className="fas fa-chevron-circle-right"></i>}
-				</button>
-				<div id='title' style={{ display: 'flex', flexDirection: 'row' }}>
-					<img alt='logo' src={logo}></img>
-					<p style={{
-						fontFamily: '\'Niramit\', sans-serif', color: 'white',
-						fontWeight: 'bold', fontSize: '30px', marginTop: '50px', marginLeft: '10px'
-					}}>Cryoinsight</p>
-				</div>
-				<div id='search-bar'>
-					<input id='input-location' style={{
-						height: '35px', marginRight: '10px',
-						paddingRight: '30px', paddingLeft: '5px'
-					}} placeholder='City name' onChange={this.props.handleInputChange} />
-					<i className="fas fa-search" style={{ position: 'relative', right: '35px',  cursor: 'pointer' }}
-						onClick={this.props.handleSearch} />
-				</div>
-			</div>
-		</>;
-	}
+
+    onKeyUpHandler(event) {
+        let code = event.which || event.keyCode;
+        if (code === 13) this.props.handleSearch();
+    }
+
+    render() {
+        return <>
+            <div className='header' style={{
+                background: 'dodgerblue', height: '80px',
+                display: 'flex', justifyContent: 'space-between', alignItems: 'center'
+            }}>
+                <button onClick={this.props.handleOpenDrawer} style={{
+                    color: 'white', background: 'none',
+                    border: 'none', outline: 'none', fontSize: '30px', paddingLeft: '10px'
+                }}>
+                    {(this.props.drawerIsOpen && <i className="fas fa-chevron-circle-down"></i>) ||
+                        <i className="fas fa-chevron-circle-right"></i>}
+                </button>
+                <div id='title' style={{ display: 'flex', flexDirection: 'row' }}>
+                    <img alt='logo' src={logo}></img>
+                    <p style={{
+                        fontFamily: '\'Niramit\', sans-serif', color: 'white',
+                        fontWeight: 'bold', fontSize: '30px', marginTop: '50px', marginLeft: '10px'
+                    }}>Cryoinsight</p>
+                </div>
+                <div id='search-bar'>
+                    <input id='input-location' onKeyDown={e => this.onKeyUpHandler(e)} style={{
+                        height: '35px', marginRight: '10px',
+                        paddingRight: '30px', paddingLeft: '5px'
+                    }} placeholder='City name' onChange={this.props.handleInputChange} />
+                    <i className="fas fa-search" style={{ position: 'relative', right: '35px', cursor: 'pointer' }}
+                        onClick={this.props.handleSearch} />
+                </div>
+            </div>
+        </>;
+    }
+
 }

447ae66673a411dbae760a08d76926c8c0067826
Giulia <giulia.coro@ennova-research.com>
2018-10-20 16:52:36 +0200

    Fixed modal close button

--- src/App.js
+++ src/App.js
@@ -24,16 +24,12 @@ class App extends React.Component {
     else this.setState({ modalIsOpen: true });
   }
 
-  createModal() {
-    return <InfoModal show={true} />;
-  }
-
   render() {
     return (<>
       <Header handleOpenDrawer={this.handleOpenDrawer.bind(this)} />
-      {this.state.drawerIsOpen && <Drawer handleOpenModal={this.handleOpenModal} />}
+      {this.state.drawerIsOpen && <Drawer handleOpenModal={this.handleOpenModal.bind(this)} />}
       <MapContainer />
-      {this.state.modalIsOpen && this.createModal()}
+      {this.state.modalIsOpen && <InfoModal show={true} handleOpenModal={this.handleOpenModal.bind(this)} />}
     </>
     );
   }
--- src/components/InfoModal.jsx
+++ src/components/InfoModal.jsx
@@ -13,12 +13,11 @@ export default class InfoModal extends React.Component {
 
 	render() {
 		return (
-			<>
 				<Modal
-					{...this.props}
 					show={this.state.show}
 					dialogClassName='custom-modal'
 					bsSize='lg'
+					onHide={() => this.props.handleOpenModal('close')}
 					style={{
 						position: 'fixed',
 						top: '50%',
@@ -33,7 +32,6 @@ export default class InfoModal extends React.Component {
 						<div>Modal content</div>
 					</Modal.Body>
 				</Modal>
-			</>
 		);
 	}
 }
